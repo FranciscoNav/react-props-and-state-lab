00833000d5cbdf2410192ac687c33765168886ac
Francisco <fnaveira11@gmail.com>
2021-04-20 15:56:44 -0400

    commit tree d2960b6685d3577a8d70595f308f5d6445bd1490

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,9 +3,11 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
+    // console.log(this.props.pets)
+    const petCards = this.props.pets.map((p) => <Pet onAdoptPet={this.props.onAdoptPet} pet={p}/> )
     return (
     <div className="ui cards">
-      <Pet onAdoptPet={this.props.onAdoptPet}/>
+      {petCards}
     </div>)
   }
 }
